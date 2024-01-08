f42fc5131dd3755334730b11db8bd1d50bd0ca0f
Abdul Rehman <87497863+AbRehman0346@users.noreply.github.com>
2024-01-08 13:50:02 +0500

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,5 @@ class Constants {
   static String CHATGPTAPI = "https://api.openai.com/v1/chat/completions";
   static String GPTMODEL = "gpt-3.5-turbo";
   static String GPTAuthorizationKey =
-      "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";
+      "GPT Key Comes Here";
 }
